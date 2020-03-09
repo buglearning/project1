8cbd86af6e42d57d50a846e7c3aa1a3e6aa3c25e
buglearning <buglearning@gmail.com>
2020-03-09 03:05:40 -0700

    third commit

--- calc.py
+++ calc.py
@@ -1,1 +1,16 @@
 #test 
+def add(x, y);
+    """Add Function"""
+    return x + y
+    
+def sub(x, y);
+    """Subtract Function"""
+    return x - y
+
+def multiply(x, y);
+    """Multiply Function"""
+    return x * y
+
+def divide(x, y);
+    """divide Function"""
+    return x / y    
